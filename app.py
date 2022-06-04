afd61791607ad05f11e64d2f379f4f5e23e20c93
sajad <mehrizis@gmail.com>
2022-06-04 14:03:43 +0200

    update app

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from fastapi import FastAPI, Query
 app = FastAPI()
 
 
-@app.get("/items/")
-async def read_items(q: list[str] | None = Query(default=None)):
-    query_items = {"q": q}
-    return query_items
+@app.get("/add/{first_number}/{second_number}")
+def add(x: float, y: float):
+    return x + y
