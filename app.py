9fddc8d27fa9c45934949fdd26f8ecb858f94a19
sajad <mehrizis@gmail.com>
2022-06-04 14:06:30 +0200

    add uvicorn

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Query
+import uvicorn
 
 app = FastAPI()
 
@@ -6,3 +7,6 @@ app = FastAPI()
 @app.get("/add/{first_number}/{second_number}")
 def add(x: float, y: float):
     return x + y
+
+if __name__=="__main__":
+    uvicorn.run(app)
